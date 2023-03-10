330901ba8df446854c6b3a2130791ec04d3cd4a8
Matias Gonzalez <mng_epson@hotmail.com>
2023-03-10 07:33:02 -0300

    [feat: add components folder and axios library]

---
+++ src/Components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Container} from '@mui/material';
+
+const Home = () => {
+  return (
+    <Container
+    maxWidth='xs'
+    sx={{
+      border:2,
+      boxShadow:1,
+      m:1
+    }}>
+<h1>Home</h1>
+    </Container>
+  )
+}
+
+export default Home
