a6ee92b457d34fdda0487c53c514aea7b65202a9
Matias Gonzalez <mng_epson@hotmail.com>
2023-03-13 09:10:01 -0300

    feat: change poke fetch

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,46 +7,22 @@ import { Button, Grid } from "@mui/material";
 
 const Home = () => {
   const [poke, setPoke] = useState([]);
-  const [nextPage, setNextPage] = useState();
-  const [prevPage, setPrevPage] = useState();
-  const [loading, setLoading] = useState(true);
   const [url, setUrl] = useState("https://pokeapi.co/api/v2/pokemon/");
 
-  const pokeData = async()=>{
-    //agrega loader
-    setLoading(true);
-    const res=await axios.get(url);
-    //setea el valor para los siguientes 20 pokemons
-    setNextPage(res.data.next);
-
-    //setea el valor para los anteriores 20 pokemons
-    setPrevPage(res.data.previous);
-
-    //trae la informacion en un array de objetos con 20 pokemons
-    getPokemon(res.data.results);
-  
-    //cancela la carga de la url
-    setLoading(false);
-  }
-
-  const getPokemon=async(res)=>{
-    res.map(async(item)=>{
-      const result = await axios.get(item.url);
-      //setea el estado de setPoke a lo que viene por result.data y devuelve el nuevo estado
-      setPoke(state=>{
-      state=[...state, result.data]
-      //se organiza el estado
-      state.sort((a,b)=>a.id>b.id?1:-1)
-      return state;
-    })
-    })
-  }
-
   useEffect(() => {
-    pokeData();
-    },[url]);
+    axios.get(url)
+    .then((res) => {
+      return res.data.results
+    })
+    .then((results) => {
+      return Promise.all(results.map((res) => axios.get(res.url)))
+    })
+    .then((results) => {
+      setPoke(results.map((res) => res.data))
+    })
+  },[url]);
 
-  // if (!poke) return <h1>Nada para ver</h1>;
+ 
   return (
       <div>
         <h1 style={{textAlign: 'center'}}>Pokemon's List</h1>
