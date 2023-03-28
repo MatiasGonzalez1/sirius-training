0111cfd71437c04e3c8fcda137e84128c7254884
Matias Gonzalez <mng_epson@hotmail.com>
2023-03-28 11:21:53 -0300

    [refactor: change select data view to list data view]

--- src/Components/PokeInfo.jsx
+++ src/Components/PokeInfo.jsx
@@ -7,6 +7,10 @@ import {
   Select,
   InputLabel,
   Typography,
+  ListItem,
+  List,
+  ListItemText,
+  ListItemButton
 } from "@mui/material";
 
 import LabelImportantIcon from "@mui/icons-material/LabelImportant";
@@ -23,6 +27,7 @@ const PokeInfo = ({
   finalEvolution,
   data,
 }) => {
+
   const alt = `img-${name}`;
   return (
     <Grid item sx={{margin: '30px 0px'}}>
@@ -43,7 +48,7 @@ const PokeInfo = ({
           </Grid> 
 
           <Grid container justifyContent="center">
-            <Grid container justifyContent="space-around" padding={1}>
+            <Grid container justifyContent="center" padding={1}>
               <Typography variant="subtitle2">Types:</Typography>
               <Grid item xs={8} textAlign="end">
                 {type?.map((one, i) => (
@@ -92,37 +97,102 @@ const PokeInfo = ({
           )}
 
           {ability ? (
-            <Grid container justifyContent="flex-start" padding={10} mt="-30px">
-              <InputLabel id="ability">Ability</InputLabel>
-              <Select fullWidth labelId="ability" label="Ability">
-                {ability?.map((one) => (
-                  <MenuItem key={one.ability.name}>{one.ability.name}</MenuItem>
-                ))}
-              </Select>
-            </Grid>
-          ) : (
-            ""
-          )}
+            // <List>
+            //  {ability.map((one)=>(
+            //   <ListItem key={one.ability.name}>
+            //   <ListItemText>
+            //   {one.ability.name}
+            //   </ListItemText>
+            // </ListItem>
+            //  ))
+            //  }
+            // </List>
+          //   <Grid container justifyContent="flex-start" padding={10} mt="-30px">
+          //     <InputLabel id="ability">Ability</InputLabel>
+          //     <ul>
+          //       {ability?.map((one) => (
+          //         <Typography key={one.ability.name}>{one.ability.name}</Typography>
+          //       ))}
+          //     </ul>
+          //   </Grid>
+          // ) : (
+          //   ""
+          <Grid container justifyContent="center">
+             <Grid item>
+                <Typography variant="subtitle2">Ability:</Typography>
+              </Grid>
+              <Grid container justifyContent="center">
+            {ability?.map((one, i)=>(
+              <Grid
+              container
+              item
+              key={i}
+              xs={5}
+              justifyContent="space-around"
+              border={1}
+              margin={1}
+            >
+              <Grid item>
+              <Typography>
+                {i +1}: {one.ability.name}
+               </Typography>
+               </Grid>
+               </Grid>
+            ))}
+          </Grid>
+          </Grid>) : ''
+          }
 
           {move ? (
-            <Grid
-              container
-              justifyContent="flex-start"
-              padding={10}
-              mt="-100px"
+            <Grid container justifyContent="center" marginTop={2}>
+            <Typography variant="subtitle2" sx={{padding:'10px', height:'40px', display:'block'}}>Moves:</Typography>
+            <List
+            sx={{
+              width: '80%',
+              maxWidth: '60%',
+              bgcolor: 'background.paper',
+              position: 'relative',
+              overflow: 'auto',
+              maxHeight: 150,
+              border: '1px solid #000',
+              borderRadius: '5px',
+              '& ul': { padding: 0 },
+            }}
             >
-              <InputLabel id="moves">Moves</InputLabel>
-              <Select fullWidth labelId="move" label="Move">
-                {move?.map((one) => (
-                  <MenuItem key={one.move.name}>{one.move.name}</MenuItem>
-                ))}
-              </Select>
+              
+             {move?.map((one, i)=>(
+              <ListItem key={one.move.name} sx={{
+                '&:hover':{
+                  backgroundColor:'#0a0a0ac7',
+                  color:'#fff'
+                  }
+                  }}>
+              <ListItemText>{i+1} - {one.move.name}
+              </ListItemText>
+            </ListItem>
+             ))
+             }
+            </List>
             </Grid>
+
+            // <Grid
+            //   container
+            //   justifyContent="flex-start"
+            //   padding={10}
+            //   mt="-100px"
+            // >
+            //   <InputLabel id="moves">Moves</InputLabel>
+            //   <Select fullWidth labelId="move" label="Move">
+            //     {move?.map((one) => (
+            //       <MenuItem key={one.move.name}>{one.move.name}</MenuItem>
+            //     ))}
+            //   </Select>
+            // </Grid>
           ) : (
             ""
           )}
 
-          <Grid container justifyContent="center" textAlign='center' padding={1}>
+          <Grid container justifyContent="center" textAlign='center' padding={1} sx={{padding:'30px'}}>
             {preEvolution? (
               <Grid item xs={6} justifyContent="center">
                 <Typography  variant="subtitle1">
