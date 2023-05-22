52a72fdd9107697ed418f77bfe79043d0083e3b6
Matias Gonzalez <mng_epson@hotmail.com>
2023-05-22 07:05:15 -0300

    [feat: finish filters]

--- src/@types/tyPesSearch.d.ts
+++ src/@types/tyPesSearch.d.ts
@@ -1,11 +1,19 @@
-export type Pokemon = {
-  id: number;
-  name: string;
-  weight: number;
-  color:Array<String>;
-  types:any;
-  type:string;
-  isBaby:any;
-  is_baby: boolean;
-};
+// export type Pokemon = {
+//   id: number;
+//   name: string;
+//   weight: number;
+//   color:Array<String>;
+//   types:any;
+//   type:string;
+//   isBaby:any;
+//   is_baby: boolean;
+// };
 
+export type Pokemon = {
+  pageNumber:number; 
+  searchName:string; 
+  minWeight:string|number; 
+  maxWeight:string|number; 
+  color:string; 
+  isBaby:boolean; 
+  type:string}
--- src/apollo-client/AdvancedSearch.ts
+++ src/apollo-client/AdvancedSearch.ts
@@ -1,28 +1,66 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 
-export const GOTTA_CATCH_THEM_FILTER =
-  gql`
-  query gottaCatchThemFilter($nameToSearch: String) {
-    pokemon_v2_pokemon(where: {name: {_ilike: $nameToSearch}} order_by: {id: asc}) {
-      name
-      id
-      weight
-      types:pokemon_v2_pokemontypes {
-        id
-        type:pokemon_v2_type {
-          name
+export const GET_POKEMONS = gql(/* GraphQL */ `
+    query GetPokemons(
+      $pageNumber: Int!, 
+      $searchName: String, 
+      $minWeight: Int!, 
+      $maxWeight: Int!, 
+      $color: String, 
+      $isBaby: Boolean, 
+      $type: [String]) {
+        pokemons: pokemon_v2_pokemon(
+          offset: $pageNumber, 
+          limit: 20, 
+          order_by: {id: asc}, 
+          where: {weight: {_gte: $minWeight, _lte: $maxWeight}, 
+                  name: {_ilike: $searchName}, 
+                  pokemon_v2_pokemonspecy: {is_baby: {_eq: $isBaby}, 
+                  pokemon_v2_pokemoncolor: {name: {_ilike: $color}}},
+                  pokemon_v2_pokemontypes: {pokemon_v2_type: {name: {_in: $type}}}}) {
+          name 
           id
-        }
-      }
-      isBaby:pokemon_v2_pokemonspecy {
-        is_baby
-        color:pokemon_v2_pokemoncolor {
-          name
-          id
-        }
-      }
+          weight
+          specy: pokemon_v2_pokemonspecy {
+            is_baby
+            color: pokemon_v2_pokemoncolor {
+              id
+              name
+              }
+            }
+          types: pokemon_v2_pokemontypes {
+            type: pokemon_v2_type {
+              id
+              name
+              }
+            }
+        } 
+        pokemonsCount: pokemon_v2_pokemon_aggregate (where: 
+          {weight: {_gte: $minWeight, _lte: $maxWeight}, 
+          name: {_ilike: $searchName}, 
+          pokemon_v2_pokemonspecy: {is_baby: {_eq: $isBaby}, 
+          pokemon_v2_pokemoncolor: {name: {_ilike: $color}}}
+          pokemon_v2_pokemontypes: {pokemon_v2_type: {name: {_in: $type}}}}) {
+            aggregate {
+              count
+            }
+          }
     }
-  }
+  `);
 
-`;
+  export const GET_COLORS_TYPES = gql`
+    query GetColorTypes {
+      color: pokemon_v2_pokemoncolor {
+        name
+        id
+      }
+      type: pokemon_v2_type {
+        id
+        name
+      }
+      
+    }
+      
+    
+  `;
